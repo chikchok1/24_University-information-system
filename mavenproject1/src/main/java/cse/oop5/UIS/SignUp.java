85e52d94ef2089fa51b2a9bcf7b39d2d5a1cdc6c
chikchok <tony09088@naver.com>
2024-11-12 01:32:40 +0900

    .

--- mavenproject1/src/main/java/cse/oop5/UIS/SignUp.java
+++ mavenproject1/src/main/java/cse/oop5/UIS/SignUp.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package cse.oop5.mavenproject1;
+package cse.oop5.UIS;
 import java.io.FileWriter;
 import java.io.IOException;
 import javax.swing.JOptionPane;
