3989443adbd3c48a9052ea9e9a45d61494511198
chikchok <tony09088@naver.com>
2024-11-12 00:54:25 +0900

    .

--- mavenproject1/src/main/java/cse/oop5/mavenproject1/SignUp.java
+++ mavenproject1/src/main/java/cse/oop5/mavenproject1/SignUp.java
@@ -10,12 +10,12 @@ import javax.swing.JOptionPane;
  *
  * @author YangJinWon
  */
-public class Membership extends javax.swing.JFrame {
+public class SignUp extends javax.swing.JFrame {
  
     /**
      * Creates new form Membership
      */
-    public Membership() {
+    public SignUp() {
         initComponents();
     }
 
@@ -198,20 +198,21 @@ public class Membership extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Membership.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Membership.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Membership.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Membership.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Membership().setVisible(true);
+                new SignUp().setVisible(true);
             }
         });
     }
