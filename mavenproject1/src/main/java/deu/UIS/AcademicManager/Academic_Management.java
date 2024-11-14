09ed994dda836c8bbc6d26c3656913ee41a40ab8
chikchok <tony09088@naver.com>
2024-11-14 16:57:11 +0900

    .

--- mavenproject1/src/main/java/deu/UIS/AcademicManager/Academic_Management.java
+++ mavenproject1/src/main/java/deu/UIS/AcademicManager/Academic_Management.java
@@ -216,9 +216,7 @@ private void loadStudentData() {
                                     .addComponent(department)
                                     .addComponent(S_number)
                                     .addComponent(name))
-                                .addGap(27, 27, 27)
-                                .addComponent(jLabel2)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGap(0, 0, Short.MAX_VALUE))
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(layout.createSequentialGroup()
@@ -250,7 +248,9 @@ private void loadStudentData() {
                                         .addComponent(S_search, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                         .addComponent(search_button, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)))))))
-                .addGap(248, 248, 248))
+                .addGap(26, 26, 26)
+                .addComponent(jLabel2)
+                .addGap(152, 152, 152))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
