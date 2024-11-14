e8de5e5a5c4217357f2e5d95a21cc87f992da76c
chikchok <tony09088@naver.com>
2024-11-15 02:48:11 +0900

    .

--- mavenproject1/src/main/java/deu/UIS/AcademicManager/Academic_Management.java
+++ mavenproject1/src/main/java/deu/UIS/AcademicManager/Academic_Management.java
@@ -5,7 +5,6 @@
 package deu.UIS.AcademicManager;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
