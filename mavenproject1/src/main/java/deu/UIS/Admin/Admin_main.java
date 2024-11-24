c26ab043fbb465d2c42af798d8b3c1a33304726f
chikchok <tony09088@naver.com>
2024-11-24 18:00:47 +0900

    .

--- mavenproject1/src/main/java/deu/UIS/Admin/Admin_main.java
+++ mavenproject1/src/main/java/deu/UIS/Admin/Admin_main.java
@@ -140,13 +140,6 @@ public class Admin_Main extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Admin_Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
